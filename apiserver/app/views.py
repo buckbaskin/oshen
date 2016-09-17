9c8006bc0923fcae365718576eb83b508334e8d6
Buck Baskin <buckbaskin@google.com>
2016-09-17 15:48:36 -0400

    Begin updating for twitterapi

--- apiserver/app/views.py
+++ apiserver/app/views.py
@@ -1,7 +1,9 @@
 from app import server
+from app.twitter_api import views
 
 from flask import render_template
 
+# a default route to display information
 @server.route('/')
 def index():
     return render_template('index.html')
