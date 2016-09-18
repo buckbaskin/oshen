27fb77c03c964799e314d10fadcad14485161d08
Buck Baskin <buckbaskin@google.com>
2016-09-18 01:21:28 -0400

    Fix maybe?

--- apiserver/app/runner/__init__.py
+++ apiserver/app/runner/__init__.py
@@ -3,6 +3,9 @@ from rq import Queue
 
 from app.util import cache
 
+host = 'localhost'
+port = 6379
+
 class QueueMock(Queue):
     def __init__():
         pass
@@ -12,18 +15,18 @@ def funnel(mock=False):
     if mock:
         return QueueMock('funnel')
     else:
-        return Queue('funnel', connection=StrictRedis(host='localhost', port=6379, db=0))
+        return Queue('funnel', connection=StrictRedis(host=host, port=port, db=0))
 
 @cache
 def twitter(mock=False):
     if mock:
         return QueueMock('twitter')
     else:
-        return Queue('twitter', connection=StrictRedis(host='127.0.0.1', port=6379, db=0))
+        return Queue('twitter', connection=StrictRedis(host=host, port=port, db=0))
 
 @cache
 def mongo(mock=False):
     if mock:
         return QueueMock('mongo')
     else:
-        return Queue('mongo', connection=StrictRedis(host='127.0.0.1', port=6379, db=0))
+        return Queue('mongo', connection=StrictRedis(host=host, port=port, db=0))
