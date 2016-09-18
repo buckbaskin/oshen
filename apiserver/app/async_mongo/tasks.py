6c4e923ff90d976ae9a4dfd362e36f80aa5aae54
Buck Baskin <buckbaskin@google.com>
2016-09-18 00:03:28 -0400

    Try async, but this isn't as nice because it won't pickle inline functions

---
+++ apiserver/app/async_mongo/tasks.py
@@ -0,0 +1,6 @@
+
+def async_find_one(dbname, collectionname, filter, max_time_ms, new_queue, new_func):
+    result = db.mongo()[dbname][collectionname].find_one(filter=filter, max_time_ms=max_time_ms)
+    new_queue.enqueue(new_function, result)
+    return 0
+
