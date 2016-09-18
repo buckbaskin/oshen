d0b702f70c688af9d553e895c182c8218406ef96
Buck Baskin <buckbaskin@google.com>
2016-09-17 22:48:08 -0400

    Get a json response! with manual data

--- apiserver/app/db/__init__.py
+++ apiserver/app/db/__init__.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson import ObjectId
 
 from app.util import cache
 
@@ -13,3 +14,11 @@ def mongo(mock=False):
         return MongoMock()
     else:
         return MongoClient('127.0.0.1', 27017)
+
+def bson2json(bson):
+    try:
+        bson['_id'] = str(bson['_id'])
+    except KeyError:
+        return bson
+    return bson
+
--- apiserver/app/views.py
+++ apiserver/app/views.py
@@ -2,7 +2,7 @@ from app import server
 from app.twitter_api import views
 from app.twitter_api import tasks as twitter_tasks
 
-from app.db import mongo
+from app.db import mongo, bson2json
 from app.runner import funnel
 
 from flask import render_template, make_response
@@ -21,15 +21,17 @@ def user_read(username):
      |- active (users that have logged in here)
     '''
     db = mongo()['users']
-    collection = db['stored_data']
+    collection = db['metadata']
     result = funnel().enqueue(twitter_tasks.user_start, username)
-    print('start request')
     request = {'username': username}
+    print('start request %s' % (request,))
     result = collection.find_one(filter=request, max_time_ms=100)
     print('result = %s' % (result,))
     if result:
-        return make_response(result, 200)
+        print('result niceness')
+        return make_response(str(bson2json(result)), 200)
     else:
+        print('unf result')
         return make_response('User Not Found', 404)
 
 @server.route('/u/<username>/basics', methods=('GET', 'POST',))
